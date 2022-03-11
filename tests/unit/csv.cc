3539cd4a34b69d287cd7db28ae3567fbba9ddec7
Bruno BELANYI <bruno@belanyi.fr>
2022-03-12 00:32:05 +0100

    tests: unit: csv: qualify 'read_csv' tests better

--- tests/unit/csv.cc
+++ tests/unit/csv.cc
@@ -5,31 +5,31 @@
 // Allow namespace pollution in tests for convenience
 using namespace kraken::csv;
 
-TEST(csv, empty) {
+TEST(read_csv, empty) {
     auto const expected = csv_type{};
     ASSERT_EQ(read_csv(""), expected);
 }
 
-TEST(csv, skip_header) {
+TEST(read_csv, skip_header) {
     auto const expected = csv_type{};
     ASSERT_EQ(read_csv("a,b,c"), expected);
 }
 
-TEST(csv, do_not_skip_header) {
+TEST(read_csv, do_not_skip_header) {
     auto const expected = csv_type{
         {"a", "b", "c"},
     };
     ASSERT_EQ(read_csv("a,b,c", CsvHeader::KEEP), expected);
 }
 
-TEST(csv, single_line) {
+TEST(read_csv, single_line) {
     auto const expected = csv_type{
         {"1", "2", "3"},
     };
     ASSERT_EQ(read_csv("a,b,c\n1,2,3"), expected);
 }
 
-TEST(csv, single_line_with_header) {
+TEST(read_csv, single_line_with_header) {
     auto const expected = csv_type{
         {"a", "b", "c"},
         {"1", "2", "3"},
