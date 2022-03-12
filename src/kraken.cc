832abfb22430c24c602adda2fcbcaec20aeef0a6
Bruno BELANYI <bruno@belanyi.fr>
2022-03-12 13:59:44 +0100

    kraken: add '--enable-trade'

--- src/kraken.cc
+++ src/kraken.cc
@@ -5,12 +5,21 @@
 #include "engine/engine.hh"
 #include "parse/parse.hh"
 
-int main() {
+int main(int argc, char** argv) {
+    auto cross_behaviour = kraken::engine::CrossBehaviour::REJECT;
+
+    if (argc > 1) {
+        using namespace std::literals;
+        if (argv[1] == "--enable-trade"sv) {
+            cross_behaviour = kraken::engine::CrossBehaviour::MATCH;
+        }
+    }
+
     auto const orders = kraken::parse::parse_orders(std::cin);
 
     auto listener = std::make_shared<kraken::engine::CsvEngineListener>();
 
-    auto engine = kraken::engine::Engine(listener);
+    auto engine = kraken::engine::Engine(listener, cross_behaviour);
 
     engine.process_orders(orders);
 
