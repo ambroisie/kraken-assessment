9550598a8701dbd72ce5ec098b929191895dcbd4
Bruno BELANYI <bruno@belanyi.fr>
2022-03-12 01:54:16 +0100

    kraken: parse: define 'Order' type
    Use strong typing for the various components of the order.

---
+++ src/parse/order.hh
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <string>
+#include <variant>
+
+#include "utils/strong-type.hh"
+
+namespace kraken::parse {
+
+/// Which side the order is on.
+enum class Side {
+    /// An offer to sell.
+    BID,
+    /// An offer to buy.
+    ASK,
+};
+
+struct PriceTag;
+/// A strongly-typed price value.
+using Price = utils::StrongType<int, PriceTag>;
+
+struct QuantityTag;
+/// A strongly-typed quantity value.
+using Quantity = utils::StrongType<int, PriceTag>;
+
+struct SymbolTag;
+/// A strongly-typed symbol value.
+using Symbol = utils::StrongType<std::string, SymbolTag>;
+
+struct UserTag;
+/// A strongly-typed user id value.
+using User = utils::StrongType<int, UserTag>;
+
+struct UserOrderIdTag;
+/// A strongly-typed user/order id value.
+using UserOrderId = utils::StrongType<int, UserOrderIdTag>;
+
+/// DTO to represent an order
+struct TradeOrder {
+    /// User of the order.
+    User user;
+    /// Symbol of the order.
+    Symbol symbol;
+    /// Price of the order.
+    Price price;
+    /// Quantity of the order.
+    Quantity quantity;
+    /// Side of the order.
+    Side side;
+    /// User/order id.
+    UserOrderId id;
+};
+
+struct CancelOrder {
+    /// User of the order.
+    User user;
+    /// User/order id to cancel.
+    UserOrderId id;
+};
+
+struct FlushOrder {};
+
+using Order = std::variant<TradeOrder, CancelOrder, FlushOrder>;
+
+} // namespace kraken::parse
