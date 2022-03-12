fa563f2f597cc522daab5a6d0c62e45595877908
Bruno BELANYI <bruno@belanyi.fr>
2022-03-12 10:51:20 +0100

    kraken: make functional binary

--- src/kraken.cc
+++ src/kraken.cc
@@ -1,5 +1,18 @@
 #include <iostream>
 
+#include "csv/write-csv.hh"
+#include "engine/csv-engine-listener.hh"
+#include "engine/engine.hh"
+#include "parse/parse.hh"
+
 int main() {
-    std::cout << "Hello World!\n";
+    auto const orders = kraken::parse::parse_orders(std::cin);
+
+    auto listener = std::make_shared<kraken::engine::CsvEngineListener>();
+
+    auto engine = kraken::engine::Engine(listener);
+
+    engine.process_orders(orders);
+
+    kraken::csv::write_csv(std::cout, listener->output());
 }
